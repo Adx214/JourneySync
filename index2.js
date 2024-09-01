5ba0a6d2eecd62bc50e241eeb5b3c4b14599ecff
Adx214 <arnab21505@gmail.com>
2024-09-01 20:28:20 +0530

    review

--- index2.js
+++ index2.js
@@ -8,3 +8,31 @@ function changeBackground() {
 }
 
 setInterval(changeBackground, 5000); // Change background every 5 seconds
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Show specific slide
+function showSlides(n) {
+    let slides = document.getElementsByClassName("review");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }    
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    // Show the current slide
+    slides[slideIndex-1].style.display = "block";  
+}
+
